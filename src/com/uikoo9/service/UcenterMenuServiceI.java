2ab837452721232031add17887449db03a81fd30
uikoo9 <uikoo9@qq.com>
2014-08-25 23:16:22 +0800

    去掉service

--- src/com/uikoo9/service/UcenterMenuServiceI.java
+++
@@ -1,9 +0,0 @@
-package com.uikoo9.service;
-
-import java.util.Map;
-
-import com.uikoo9.util.crud.QPage;
-
-public interface UcenterMenuServiceI {
-	public QPage list(Map<String, String[]> paras, String tableName);
-}
