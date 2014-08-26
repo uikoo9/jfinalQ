9f5b1e474122981af69239858ae27a769739e5c0
uikoo9 <uikoo9@qq.com>
2014-08-26 16:20:44 +0800

    qmask z-index 2001

--- WebRoot/WUI/qmask/qmask.js
+++ WebRoot/WUI/qmask/qmask.js
@@ -2,11 +2,11 @@ function Qmask(options){
 	var o = new Object();
 	
 	o.background = document.createElement('div');
-	o.background.style.cssText = 'display:none;position:absolute;top:0;left:0;width:100%;height:100%;background-color:white;z-index:1001;';
+	o.background.style.cssText = 'display:none;position:absolute;top:0;left:0;width:100%;height:100%;background-color:white;z-index:2001;';
 	
 	o.loading = document.createElement('img');
 	o.loading.src = base + '/WUI/qmask/loading.gif';
-	o.loading.style.cssText = 'display:none;position:absolute;top:50%;left:50%;width:200px;height:200px;margin:-100px 0 0 -100px;z-index:1002;overflow:auto;';
+	o.loading.style.cssText = 'display:none;position:absolute;top:50%;left:50%;width:200px;height:200px;margin:-100px 0 0 -100px;z-index:2002;overflow:auto;';
 	
 	o.qmask = function(){
 		this.background.style.display = 'block';
