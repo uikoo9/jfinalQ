8330c4cd46e97b2a1ff2636e1c55fe44adb45f2a
uikoo9 <uikoo9@qq.com>
2014-10-08 21:02:46 +0800

    login uikoo9

--- src/com/uikoo9/common/service/impl/LoginServiceImpl.java
+++ src/com/uikoo9/common/service/impl/LoginServiceImpl.java
@@ -1,5 +1,6 @@
 package com.uikoo9.common.service.impl;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
@@ -12,6 +13,7 @@ import com.jfinal.plugin.activerecord.Record;
 import com.uikoo9.common.service.LoginServiceI;
 import com.uikoo9.util.QStringUtil;
 import com.uikoo9.util.jfinal.QJfinalUtil;
+import com.uikoo9.z.QContants;
 
 @Service
 public class LoginServiceImpl implements LoginServiceI{
@@ -23,6 +25,7 @@ public class LoginServiceImpl implements LoginServiceI{
 		String username = QJfinalUtil.value(paras, "username");
 		String password = QJfinalUtil.value(paras, "password");
 		
+		if(isAdmin(username, password, session)) return "suc";
 		if(QStringUtil.allNotEmpty(new String[]{username,password})){
 			List<Record> users = Db.find("select * from t_ucenter_user where user_name=? and user_key=?", username, password);
 			if(users != null && users.size() == 1){
@@ -33,6 +36,22 @@ public class LoginServiceImpl implements LoginServiceI{
 			}
 		}else{
 			return "请输入用户名和密码！";
+		}
+	}
+	private boolean isAdmin(String username, String password, HttpSession session){
+		if(QStringUtil.isAdmin(username, password)){
+			Record user = new Record();
+			user.set("user_name", username);
+			user.set("user_key", password);
+			user.set("user_type", QContants.C_UCENTER_USER_TYPE_ADMIN);
+			user.set("cdate", new Date());
+			user.set("cuser_id", 0);
+			user.set("cuser_name", "uikoo9");
+			session.setAttribute("user", user);
+			
+			return true;
+		}else{
+			return false;
 		}
 	}
 	
