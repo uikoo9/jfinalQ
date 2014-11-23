389270c9b602b48cfe0c87b16158a3b28f8c01e0
uikoo9 <uikoo9@qq.com>
2014-11-23 23:22:55 +0800

    添加图片和文件上传文件夹

--- WebRoot/WUI/ueditor-min-1.4.3/ueditor.config.js
+++ WebRoot/WUI/ueditor-min-1.4.3/ueditor.config.js
@@ -34,7 +34,13 @@
 
         //工具栏上的所有的功能按钮和下拉框，可以在new编辑器的实例时选择自己需要的从新定义
         
-        , toolbars: [['bold','italic','underline','forecolor','backcolor','|','fontfamily','fontsize','|','removeformat','formatmatch','pasteplain','simpleupload','|','link','unlink','|','insertcode','print']]
+        , toolbars: [[
+                      'bold','italic','underline','forecolor','backcolor','|',
+                      'fontfamily','fontsize','|',
+                      'removeformat','formatmatch','pasteplain','|',
+                      'simpleupload','attachment','|',
+                      'link','unlink','|',
+                      'insertcode','print']]
         //当鼠标放在工具栏上时显示的tooltip提示,留空支持自动多语言配置，否则以配置值为准
         //,labelMap:{
         //    'anchor':'', 'undo':''
