408b17a6d444cda959bc4c2dc5fe30cf8ef0cd3f
uikoo9 <uikoo9@qq.com>
2014-11-24 09:50:08 +0800

    百度编辑器工具栏修改

--- WebRoot/WUI/ueditor-min-1.4.3/ueditor.config.js
+++ WebRoot/WUI/ueditor-min-1.4.3/ueditor.config.js
@@ -39,8 +39,8 @@
                       'fontfamily','fontsize','|',
                       'removeformat','formatmatch','pasteplain','|',
                       'simpleupload','attachment','|',
-                      'link','unlink','|',
-                      'insertcode','print']]
+                      'source','link','unlink','|',
+                      'insertcode']]
         //当鼠标放在工具栏上时显示的tooltip提示,留空支持自动多语言配置，否则以配置值为准
         //,labelMap:{
         //    'anchor':'', 'undo':''
