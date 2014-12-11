4a1b82c276f927797ac8730ceb99c21761d5f900
uikoo9 <uikoo9@qq.com>
2014-12-11 19:08:20 +0800

    权限封装ok

--- src/com/uikoo9/Test.java
+++
@@ -1,9 +0,0 @@
-package com.uikoo9;
-
-import com.uikoo9.util.file.QQiNiuUtil;
-
-public class Test {
-	public static void main(String[] args) {
-		QQiNiuUtil.uploadFile("/test/1.png", "e:/1.png");
-	}
-}
